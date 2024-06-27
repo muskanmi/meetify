4b74157b959c2a5b7399f872dd6afa34f3ceeae5
Muskan Mittal <mittalmuskan758@gmail.com>
2024-06-27 09:20:58 +0530

    styling of login and signup page

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const SignUpPage = () => {
     return (
-        <main>
+        <main className='flex h-screen w-full items-center justify-center'>
             <SignUp />
         </main>
     )
