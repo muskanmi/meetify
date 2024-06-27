3068587fe4f6c7ef4c5a94424e0889ee7374764a
Muskan Mittal <mittalmuskan758@gmail.com>
2024-06-27 21:56:15 +0530

    working on stream chat api

---
+++ providers/StreamClientProvider.tsx
@@ -0,0 +1,9 @@
+import { StreamVideo, StreamVideoClient } from "@stream-io/video-react-sdk";
+
+const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
+
+const StreamVideoProvider = () => {
+  return <StreamVideo client={client}></StreamVideo>;
+};
+
+export default StreamVideoProvider;
